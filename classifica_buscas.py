931b25981d71fd5858008bb6624b712b9038f5b3
leandrofreire <rafaelleandrofreire@gmail.com>
2019-03-15 18:52:47 -0300

    Funcao para verificar qual algoritmo é melhor para a classificacao e qual sua porcentagem de predição

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -17,16 +17,25 @@ Ydummies_df = Y_df
 X = Xdummies_df.values
 Y = Ydummies_df.values
 
-porcentagem_treino = 0.9
+porcentagem_treino = 0.8
+porcentagem_teste = 0.1
 
 tamanho_treino = int(porcentagem_treino * len(Y))
-tamanho_teste = len(Y) - tamanho_treino
+tamanho_teste = int(porcentagem_teste * len(Y))
+tamanho_validacao = len(Y) - tamanho_treino - tamanho_teste
 
+# 0 até 799
 treino_dados = X[:tamanho_treino]
 treino_marcacoes = Y[:tamanho_treino]
 
-teste_dados = X[-tamanho_teste:]
-teste_marcacoes = Y[-tamanho_teste:]
+# 800 até 899
+fim_de_teste = tamanho_treino + tamanho_teste
+teste_dados = X[tamanho_treino: fim_de_teste]
+teste_marcacoes = Y[tamanho_treino: fim_de_teste]
+
+# 900 até 999
+validacao_dados = X[fim_de_teste:]
+validacao_marcacoes = Y[fim_de_teste:]
 
 def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes):
     modelo.fit(treino_dados, treino_marcacoes)
@@ -42,20 +51,39 @@ def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes, teste_dados, t
 
     msg = "Taxa de acerto do algoritmo {0}: {1}".format(nome, taxa_acerto)
     print(msg)
+    return taxa_acerto
+
+
+def teste_real(modelo, validacao_dados, validacao_marcacoes):
+    resultado = modelo.predict(validacao_dados)
+    acertos = resultado == validacao_marcacoes
+
+    total_acertos = sum(acertos)
+    total_elementos = len(validacao_marcacoes)
+    taxa_acerto = 100.0 * total_acertos / total_elementos
+    msg = "Taxa de acerto do vencedor entre os dois algoritmos no mundo real: {0}".format(taxa_acerto)
+    print(msg)
 
 from sklearn.naive_bayes import MultinomialNB
-modelo = MultinomialNB()
-fit_and_predict("MultinomialNB", modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
+modeloMultinomial = MultinomialNB()
+resultadoMultinomial = fit_and_predict("MultinomialNB", modeloMultinomial, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
 
 from sklearn.ensemble import AdaBoostClassifier
-modelo = AdaBoostClassifier()
-fit_and_predict("AdaBoostClassifier", modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
+modeloAdaBoost = AdaBoostClassifier()
+resultadoAdaBoost = fit_and_predict("AdaBoostClassifier", modeloAdaBoost, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
+
+if resultadoMultinomial > resultadoAdaBoost:
+    vencedor = modeloMultinomial
+else:
+    vencedor = modeloAdaBoost
+
+teste_real(vencedor, validacao_dados, validacao_marcacoes)
 
 
 # Acerto base recebe o valor maximo que foi encontrado na soma do array
-acerto_base = max(Counter(teste_marcacoes).values())
+acerto_base = max(Counter(validacao_marcacoes).values())
 # Calcula a porcentagem de acerto, pega a quantidade encontrada no array e divide pelo tamanho
-taxa_acerto_base = 100.0 * acerto_base / len(teste_marcacoes)
+taxa_acerto_base = 100.0 * acerto_base / len(validacao_marcacoes)
 # retorna o maior entre eles utilizando função max()
 print("Taxa de acerto base: %.2f" % taxa_acerto_base)
 
